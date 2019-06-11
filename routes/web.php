027cb28adee75e09397f8cc935036cdec5ff1947
Sidrit Trandafili <forge405@gmail.com>
2019-06-11 11:27:12 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// User - ACL - ROLES/PERMISSIONS
+    // perid - univoco
+    // ruolo -
+// Album
+    // Ha tantye foto / video
+    // Ha tag
+    // appartiente ad un utente
+    // pu`o essere privato o meno
+// Media - photo / video
+    // hanno tag
+    // appartengno a piu album
+    // hanno un autore
+    // data di scattto
+    // pu`o essere privato o meno
+// Tag
+    // appanrtono a piu media
+    // appartengono a piu album
